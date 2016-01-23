d6e5aa91b89fd00d8511d9fa8ed2ecbe08708012
bruce <darkoverlordofdata@gmail.com>
2016-01-22 20:41:59 -0800

    init

---
+++ 02_getting_an_image_on_the_screen/build/CMakeFiles/ui_copy_02_getting_an_image_on_the_screen.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  )
+# The set of files for implicit dependencies of each language:
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "GETTEXT_PACKAGE=\"02_getting_an_image_on_the_screen\""
+  "LOCALE_DIR=\"/usr/local/share/locale\""
+  "PACKAGE_DATA_DIR=\"/usr/local/share/02_getting_an_image_on_the_screen\""
+  "PACKAGE_NAME=\"02_getting_an_image_on_the_screen\""
+  "PACKAGE_UI_DIR=\"/usr/local/share/02_getting_an_image_on_the_screen/ui\""
+  "PACKAGE_VERSION=\"0.0.0\""
+  "PIXMAP_DIR=\"/usr/local/share/pixmaps/02_getting_an_image_on_the_screen\""
+  "VALA_VERSION=\"0.28\""
+  "_REENTRANT"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
